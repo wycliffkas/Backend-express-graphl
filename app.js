ccb3a858572096bf12b68b9ee05d8c935e61db61
wycliffkas <w4wycliff@gmail.com>
2020-06-27 07:57:13 +0300

    delete

--- app.js
+++ app.js
@@ -45,8 +45,8 @@ app.use("/images", express.static(path.join(__dirname, "images")));
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    "Access-Contorl-Allow-Methods",
-    "POST, GET, PUT, PATCH, DELETE"
+    "Access-Control-Allow-Methods",
+    "POST,GET,PUT,PATCH,DELETE"
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
