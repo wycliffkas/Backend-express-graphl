7f844f2c25a075193a259a244473862b823d2bdc
wycliffkas <w4wycliff@gmail.com>
2020-06-27 06:59:13 +0300

    create, get and edit posts

--- routes/feed.js
+++ routes/feed.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const { body } = require("express-validator/check");
+
 const router = express.Router();
 
 const feedController = require("../controllers/feed");
@@ -8,6 +10,20 @@ const feedController = require("../controllers/feed");
 router.get("/posts", feedController.getPosts);
 
 //POST /feed/post
-router.post("/post", feedController.createPost);
+router.post(
+  "/post",
+  [
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  feedController.createPost
+);
+
+router.get("/post/:postId", feedController.getPost);
+
+router.put("/post/:postId", [
+  body("title").trim().isLength({ min: 5 }),
+  body("content").trim().isLength({ min: 5 }),
+], feedController.updatePost);
 
 module.exports = router;
